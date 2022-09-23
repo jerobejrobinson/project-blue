a67d38a01270f430f13e0bb75a1305163cdf1160
jerobejrobinson <jerobejrobinson@gmail.com>
2022-09-23 16:57:42 -0500

    update

--- components/Buttons/LoginBtn.js
+++ components/Buttons/LoginBtn.js
@@ -32,7 +32,7 @@ export default function LoginBtn() {
         <>
             <button 
                 onClick={(e) => handleClick(e)}
-                className='p-2 bg-blue-600 text-white rounded'
+                className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
             >
                 Login
             </button>
