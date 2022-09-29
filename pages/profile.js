02db7a87a8ce527cc68412486230f51b87a3a9b1
jerobejrobinson <jerobejrobinson@gmail.com>
2022-09-29 13:48:25 -0500

    doing something

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,18 @@
 import LogoutBtn from "components/Buttons/LogoutBtn";
-
+import { supabase } from "config/supabase/client";
 export default function Profile({ user }) {
   return (
     <div>
-      hi {user.email}
+      hi 
       <LogoutBtn />
     </div>
   )
 }
 
+export async function getServerSideProps(context) {
+  const session = supabase.auth.session()
+  console.log(session)
+  return {
+    props: {}
+  }
+}
